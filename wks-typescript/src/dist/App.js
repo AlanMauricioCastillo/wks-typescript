755f05a04b255028a7f9401c63a825b75a9188be
Mauricio Alan Castillo <79920621+AlanMauricioCastillo@users.noreply.github.com>
2021-11-05 16:55:03 -0300

    fixed!

---
+++ wks-typescript/src/dist/App.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+require("./App.css");
+function App(_a) {
+    var title = _a.title;
+    return react_1["default"].createElement("div", null,
+        "Henry Workshop - ",
+        title);
+}
+exports["default"] = App;
